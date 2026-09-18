e82bef568c9047bfb53b8bb70e43b92ff5dec02a
velikrdy <velik320@gmail.com>
2026-09-18 23:25:49 +0300

    Update settings_screen.dart

--- lib/ui/settings_screen.dart
+++ lib/ui/settings_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dev.dart' if (dart.library.html) 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import '../services/settings_service.dart';
 
